42237d3e4980624b42407c1fa7d4df27cb0fc6ec
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2022-12-14 15:48:17 +0500

    add Models

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import sequelize from './db.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import sequelize from './db.js';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync()
+    await sequelize.authenticate();
+    await sequelize.sync();
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
   app.listen(PORT, () => {
-    console.log(`server start at port:${PORT}`)
-  })
-}
-start()
+    console.log(`server start at port:${PORT}`);
+  });
+};
+start();
