a9a8a531b943068d15d0dfd686e1d3e4574a764b
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-02-06 18:52:16 +0500

    repair controller 36:56 stop

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,15 @@
 import {ApiError} from '../error/ApiError.js';
 
 function userController() {
-  const {badRequest, internal, forbidden} = ApiError;
+  const {badRequest, internal, forbidden} = ApiError();
   const registration = async (req, res) => res.json('registration');
   const login = async (req, res) => res.json('login');
   const check = async (req, res, next) => {
     const {id} = req.query;
     if (!id) {
-      return next(/* badRequest('не задан ID') */);
+      return next(badRequest(``, `не задан ID`));
     }
-    res.json({check: 'check'});
+    return res.json(id);
   };
   return {registration, login, check};
 }
