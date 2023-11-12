0e76aec7ce48bc043534be5835bd591a46d94de6
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-11-12 21:57:48 +0500

    cloud db connected

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 
 const app = express();
 dotenv.config()
 const PORT= parseInt(process.env.SERVER_PORT || "")||7001
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+app.get("/users", async (_, res) => {
+  const users = await prisma.device.findFirst();
+  res.json(users);
 });
 
 ViteExpress.listen(app, PORT, () => {
