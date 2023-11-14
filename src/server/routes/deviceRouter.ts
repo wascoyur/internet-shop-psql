41120f3e0323c29bfc986a528cb8a220eae97b1e
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2023-11-14 18:01:29 +0500

    добавлены контроллеры

--- src/server/routes/deviceRouter.ts
+++ src/server/routes/deviceRouter.ts
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.post('/create', deviceController.createDevice);
 router.get('/devices', deviceController.getDevices);
-router.get('/devices/:id', deviceController.getDevices);
 router.patch('/update', deviceController.updateDevice);
 router.delete('/delete', deviceController.deleteDevice);
 
