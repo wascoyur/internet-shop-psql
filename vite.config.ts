4710458bcb0cdfbaafb98901ce36f07a8c933eff
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2024-01-06 16:37:10 +0500

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "src/client/shared"),
+      "@app": path.resolve(__dirname, "src/client/app"),
+      "@widgets": path.resolve(__dirname, "src/client/widgets"),
+      "@feauters": path.resolve(__dirname, "src/client/feauters"),
+      "@entities": path.resolve(__dirname, "src/client/entities"),
+    },
+  },
 });
