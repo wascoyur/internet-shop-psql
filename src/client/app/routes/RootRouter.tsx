fa81b6a995672a65cb52113db474fb3f4526a475
yuriy.vasilev <yuriy.vasilev@rtkit.ru>
2024-01-07 19:38:47 +0500

    client: add pages

--- src/client/app/routes/RootRouter.tsx
+++ src/client/app/routes/RootRouter.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import {createBrowserRouter}from "react-router-dom";
-import {SignIn, SignUp} from '../../feauters/auth/authRoutes';
+import {RouteSignIn, RouteSignUp} from '../../feauters/auth/authRoutes';
 import {ErrorPage} from '../ErrorPage';
 import App from '../App';
+import Bucket from '../../pages/Bucket';
+import Payment from '../../pages/Payment';
 
-
+export const RouteBucket={
+  path: "/bucket",
+  element: <Bucket />,
+  errorElement: <ErrorPage />,
+}
+export const RoutePayment={
+  path: "/payment",
+  element: <Payment />,
+  errorElement: <ErrorPage />,
+}
 export const Root = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />
   },
-  SignUp,
-  SignIn,
+  RouteSignUp,
+  RouteSignIn,
+  RouteBucket,
+  RoutePayment
 ]);
--- src/client/feauters/auth/authRoutes.tsx
+++ src/client/feauters/auth/authRoutes.tsx
@@ -3,12 +3,12 @@ import {ErrorPage} from '../../app/ErrorPage';
 import RegistrationForm from './RegistrationForm';
 import {LoginForm} from './LoginForm';
 
-export const SignUp={
+export const RouteSignUp={
   path: "/register",
   element: <RegistrationForm />,
   errorElement: <ErrorPage />,
 }
-export const SignIn={
+export const RouteSignIn={
   path: "/login",
   element: <LoginForm />,
   errorElement: <ErrorPage />,
