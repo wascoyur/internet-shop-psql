813ed545518d25b91b18d65ea4e9143c20fbd23c
vasilev,yurii.h3007 <mytem20007@gmail.com>
2024-07-07 05:42:26 +0500

    добавлена валидация полей

--- src/widgets/user-widget/userWigetHelpers.ts
+++ src/widgets/user-widget/userWigetHelpers.ts
@@ -10,3 +10,61 @@ export const getPermissions = (props: Roles): string[] => {
   }
   return permissions;
 };
+export const getFormData = (formData: FormData) => {
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const firstName = formData.get("firstname");
+  const lastName = formData.get("lastname");
+  const roles = formData.get("roles");
+  const phone = formData.get("phone");
+
+  return {
+    email,
+    password,
+    firstName,
+    lastName,
+    roles,
+    phone,
+  };
+};
+
+export const validateFields = (formData: FormData): boolean => {
+  const requiredFields = [
+    "email",
+    "password",
+    "firstname",
+    "lastname",
+    "roles",
+    "phone",
+  ];
+
+  for (const field of requiredFields) {
+    if (!formData.has(field)) {
+      return false; // Если хотя бы одно обязательное поле отсутствует
+    }
+
+    if (field === "roles") {
+      //TODO: проверить роли
+      const roles = formData.get("roles");
+      return rolesVerify(roles);
+    }
+  }
+
+  function rolesVerify(arg: FormDataEntryValue | null) {
+    const allowedVals = ["A", "U", "M", "O"].sort();
+    if (typeof arg === "string") {
+      const rolesValue = arg.toUpperCase().split("").sort();
+
+      return rolesValue.length > 0
+        ? rolesValue.every((role) => {
+            return allowedVals.some((allowRole) => {
+              return allowRole === role;
+            });
+          })
+        : false;
+    }
+    return false;
+  }
+
+  return true; // Все обязательные поля присутствуют
+};
