58ee71fad92ecafa8554c4a84a748982a8e8f835
vasilev,yurii.h3007 <mytem20007@gmail.com>
2024-07-09 07:41:24 +0500

    исправлено получение данных формы

--- src/widgets/user-widget/userWigetHelpers.ts
+++ src/widgets/user-widget/userWigetHelpers.ts
@@ -1,8 +1,10 @@
 import { Roles } from "../../app/types/user.ts";
+import { UserData } from "./FormInput.tsx";
 
 export const isAnyRolesIsExist = (props: Roles) => {
   return Object.keys(props).length;
 };
+
 export const getPermissions = (props: Roles): string[] => {
   const permissions: string[] = [];
   for (const key in props) {
@@ -10,34 +12,40 @@ export const getPermissions = (props: Roles): string[] => {
   }
   return permissions;
 };
-export const getFormData = (formData: FormData) => {
-  const email = formData.get("email");
-  const password = formData.get("password");
-  const firstName = formData.get("firstname");
-  const lastName = formData.get("lastname");
-  const roles = formData.get("roles");
-  const phone = formData.get("phone");
 
-  return {
-    email,
-    password,
-    firstName,
-    lastName,
-    roles,
-    phone,
+const requiredFields = [
+  "email",
+  "password",
+  "firstname",
+  "lastname",
+  "roles",
+  "phone",
+  "name",
+];
+
+export const getFormData = (formData: FormData): UserData | undefined => {
+  // @ts-expect-error неизвестная ошибка
+  let data: UserData = {};
+
+  const isKeyRequired = (key: string) => {
+    return requiredFields.some((requiredField) => requiredField === key);
   };
+
+  const isValueString = (fieldValue: FormDataEntryValue): boolean => {
+    return typeof fieldValue === "string" && fieldValue.length > 0;
+  };
+
+  for (const [key, value] of formData.entries()) {
+    if (!isKeyRequired(key) || !isValueString(value)) {
+      return undefined;
+    }
+    data = { ...data, [key]: value };
+  }
+
+  return data;
 };
 
 export const validateFields = (formData: FormData): boolean => {
-  const requiredFields = [
-    "email",
-    "password",
-    "firstname",
-    "lastname",
-    "roles",
-    "phone",
-  ];
-
   for (const field of requiredFields) {
     if (!formData.has(field) || !isExistValue(field)) {
       return false; // Если хотя бы одно обязательное поле отсутствует
