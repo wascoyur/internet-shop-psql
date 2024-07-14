ae7d4c767eed81ff35c025eaf2ff7b7758aa3fbc
vasilev,yurii.h3007 <mytem20007@gmail.com>
2024-07-14 20:44:05 +0500

    непонятная ошибка создания юзера

--- src/shared/api/accountService.ts
+++ src/shared/api/accountService.ts
@@ -1,17 +1,25 @@
 import { User } from "../../app/types/user.ts";
 
 export const addUser = async (props: Omit<User, "id"> | undefined) => {
-  const URL = `${import.meta.env.VITE_APP_DEV_API}/account/user`;
-  console.log(URL);
+  const URL = `http://${import.meta.env.VITE_APP_DEV_API}/account/user`;
   if (!props) return;
 
   try {
     const response = await fetch(URL, {
       method: "POST",
-      body: JSON.stringify({ props }),
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...props,
+      }),
     });
+
+    console.log(await response.json());
+
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    return response.json();
+    return await response.json();
   } catch (e) {
     console.error("There was a problem with the fetch operation: ", e);
     return null;
